8eb6689a6a4ee233d8e641042728d8b58550b9a9
Maxime Baudot (MXBA) <mxba@oticonmedical.com>
2023-12-12 14:26:25 +0100

    mxba

--- MXBA/day8_part2.py
+++ MXBA/day8_part2.py
@@ -8,7 +8,7 @@ from dataclasses import dataclass
 from pprint import pprint as pp
 
 infile = "data_day8.txt"
-infile = "exemple.txt"
+# infile = "exemple.txt"
 
 
 @dataclass()
@@ -17,17 +17,19 @@ class Node:
     left: str
     right: str
 
-    def move(self, direction):
+    def __post_init__(self):
+        self.is_start = self.node_name[-1] == "A"
+        self.is_end = self.node_name[-1] == "Z"
+
+    def child(self, direction):
         if direction == "L":
             return self.left
         else:
             return self.right
 
-    def is_start(self):
-        return self.node_name[-1] == "A"
-
-    def is_end(self):
-        return self.node_name[-1] == "Z"
+    def update(self, left_node, right_node):
+        self.left = left_node
+        self.right = right_node
 
 
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), infile), "r") as puzzle_input:
@@ -35,10 +37,11 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), infile), "r")
     # split puzzle input into data and maps
     lines = [x.strip() for x in puzzle_input.readlines()]
 
-    # first line: directionx
+    # first line: directions
     directions = lines[0]
 
     # nodes
+    print("Read nodes...")
     nodes = {}
     for line in lines[2:]:
         match = re.match("(...) = \((...), (...)\)", line)
@@ -46,25 +49,27 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), infile), "r")
         node = Node(node_name, left, right)
         nodes[node.node_name] = node
         # print(node)
-
     # pp(nodes)
 
+    # update Nodes with the real child nodes
+    print("Update nodes...")
+    for node in nodes.values():
+        left_node = nodes[node.child("L")]
+        right_node = nodes[node.child("R")]
+        node.update(left_node, right_node)
+
+    # now, all Node has Nodes as Left and Right children
     nb_steps = 0
-    current_nodes = [x for x in nodes.values() if x.is_start()]
-    # print("\nSTART")
-    pp(current_nodes)
+    current_nodes = [x for x in nodes.values() if x.is_start]
+    print("\nSTART")
+    # pp(current_nodes)
     direction_it = itertools.cycle(directions)
 
-    while False in [node.is_end() for node in current_nodes]:
+    while False in [node.is_end for node in current_nodes]:
         next_dir = next(direction_it)
-        current_nodes = [nodes[node.move(next_dir)] for node in current_nodes]
-    #     node = nodes[current_node]
-    #     new_node = node.move(next_dir)
+        current_nodes = [node.child(next_dir) for node in current_nodes]
         nb_steps += 1
-        # print(f"\n{next_dir}")
-        # pp(current_nodes)
-    #     current_node = new_node
         if (nb_steps % 100_000) == 0:
-            print(f"=> {nb_steps}")
+            print(f"=> {nb_steps:_}")
 
     print(f"\n{nb_steps=}")
